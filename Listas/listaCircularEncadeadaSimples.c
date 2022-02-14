f6b8aaea8e1da65498764d50ac5c196b779a948e
Wesley Santos <wesleysotnas64@gmail.com>
2022-02-14 11:37:13 -0300

    Update listaCircularEncadeadaSimples.c

--- Listas/listaCircularEncadeadaSimples.c
+++ Listas/listaCircularEncadeadaSimples.c
@@ -12,9 +12,27 @@ int tamanho(NO *ptInicio);
 int buscar(int chave, NO *ptInicio);
 void addNOInicio(int chave, NO **ptInicio);
 
+void imprimeListaRecursiva(NO *ptInicio);
+
 int main(){
 
-    NO *ptInicio;
+    NO *ptInicio = NULL;
+
+    addNOInicio(12, &ptInicio);
+    addNOInicio(11, &ptInicio);
+    addNOInicio(10, &ptInicio);
+    addNOInicio(9, &ptInicio);
+    addNOInicio(8, &ptInicio);
+    addNOInicio(7, &ptInicio);
+    addNOInicio(6, &ptInicio);
+    addNOInicio(5, &ptInicio);
+    addNOInicio(4, &ptInicio);
+    addNOInicio(3, &ptInicio);
+    addNOInicio(2, &ptInicio);
+    addNOInicio(1, &ptInicio);
+    
+
+    //imprimeListaRecursiva(ptInicio);
 
     return 0;
 }
@@ -44,34 +62,53 @@ int tamanho(NO *ptInicio){
 
 int buscar(int chave, NO *ptInicio){ //O(n)
     if(ptInicio != NULL){
+        
         NO *ptAux = ptInicio;
 
         do{
             if(ptAux->chave == chave) return 1;
             else ptAux = ptAux->prox;
-        } while(ptAux != ptInicio);
+        } while(ptAux != ptInicio);    
+    }
 
-        printf("Chave %d não encontrado!\n", chave);
-        return 0;
-        
-    } else{
-        printf("Chave %d não encontrado!\n", chave);
-        return 0;
-    } 
+    printf("Chave %d não encontrado!\n", chave);
+    return 0;
 }
 
-void addNOInicio(int chave, NO **ptInicio){
+void addNOInicio(int chave, NO **ptInicio){ //O(1)
     printf("-----------------------\n");
     printf("ADD_INICIO. CHAVE = %d\n", chave);
 
     if(buscar(chave, (*ptInicio)) == 0){
+        
         NO **no  = malloc(sizeof(NO*));
         (*no) = criaNO(chave);
 
-        (*no)->prox = (*ptInicio);
-        (*ptInicio) = (*no);
+        if(tamanho((*ptInicio)) == 0){
+            (*ptInicio) = (*no);
+            (*no)->prox = (*ptInicio);
+        } else{
+            
+            NO *ptAux = (*ptInicio)->prox;
+            while(1){
+                if(ptAux->prox == (*ptInicio)) break;
+                else ptAux = ptAux->prox;
+            }
+            (*no)->prox = (*ptInicio);
+            (*ptInicio) = (*no);
+            ptAux->prox = (*ptInicio);
+        }
+
+        
 
         printf("Chave %d adicionada!\n", chave);
     } else printf("Chave %d existe! Não adicionado!\n", chave);
     printf("-----------------------\n");
+}
+
+void imprimeListaRecursiva(NO *ptInicio){
+    if(ptInicio != NULL){
+        printf("[%2d] -> %2d\n",ptInicio->chave, ptInicio->prox->chave);
+        if(ptInicio->prox != ptInicio) imprimeListaRecursiva(ptInicio->prox);
+    } 
 }
