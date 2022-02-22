6ae27f1f85d756af5c1ab4d5f0163597fd9eb56f
Wesley Santos <wesleysotnas64@gmail.com>
2022-02-22 07:41:22 -0300

    Update bubbleSort.c
    bubbleSort funcionando corretamente;

--- AlgoritmosDeOrdenacao/bubbleSort.c
+++ AlgoritmosDeOrdenacao/bubbleSort.c
@@ -11,17 +11,20 @@ int main(){
 
     int vetor[TAMANHO];
 
-    vetor[0] = 10;
-    vetor[1] = 9;
-    vetor[2] = 8;
-    vetor[3] = 7;
-    vetor[4] = 6;
-    vetor[5] = 5;
-    vetor[6] = 4;
-    vetor[7] = 3;
-    vetor[8] = 2;
-    vetor[9] = 1;
+    vetor[0] = 33;
+    vetor[1] = 12;
+    vetor[2] = 99;
+    vetor[3] = 54;
+    vetor[4] = 42;
+    vetor[5] = 87;
+    vetor[6] = 15;
+    vetor[7] = 10;
+    vetor[8] = 96;
+    vetor[9] = 81;
 
+    imprimeVetor(vetor, TAMANHO);
+
+    bubbleSort(vetor, TAMANHO);
     imprimeVetor(vetor, TAMANHO);
 
     return 0;
@@ -30,22 +33,17 @@ int main(){
 
 void bubbleSort(int vetor[], int tamanho){//O(n^2)
     for(int i = 0; i < tamanho; i++){
-        for(int j = 0; j < tamanho-i; j++)if(vetor[j] > vetor[j+1]) troca(vetor, j, j+1);
+        for(int j = 0; j < tamanho-i-1; j++) if(vetor[j] > vetor[j+1]) troca(vetor, j, j+1);
     }
 }
 
 void troca(int vetor[], int posi1, int posi2){ //O(1)
-    printf("TROCA: [%2d] POR [%2d]\n", vetor[posi1], vetor[posi2]);
     int aux = vetor[posi1];
     vetor[posi1] = vetor[posi2];
     vetor[posi2] = aux;
 }
 
 void imprimeVetor(int vetor[], int tamanho){ //O(n)
-    printf("-------------------\n");
-    printf("IMPRIMINDO VETOR!\n");
-
-    for(int i = 0; i < tamanho; i++) printf("%2d -> [%2d]\n", i, vetor[i]);
-
-    printf("-------------------\n");
+    for(int i = 0; i < tamanho; i++) printf("[%2d] ", vetor[i]);
+    printf("\n");
 }
