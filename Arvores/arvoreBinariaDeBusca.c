97f3acbae401e2a1320b28bca760bed7e0ef4e00
Wesley Santos <wesleysotnas64@gmail.com>
2022-02-23 05:59:55 -0300

    Update arvoreBinariaDeBusca.c
    Função 'CriaNO()' adicionada

--- Arvores/arvoreBinariaDeBusca.c
+++ Arvores/arvoreBinariaDeBusca.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no NO;
+struct no{
+    int chave;
+    NO *esq;
+    NO *dir;
+};
+
+NO *criaNO(int chave);
+
 int main(){
     return 0;
+}
+
+NO *criaNO(int chave){
+    NO *novo = malloc(sizeof(NO));
+
+    novo->chave = chave;
+    novo->esq  = NULL;
+    novo->dir  = NULL;
+
+    return novo;
 }
