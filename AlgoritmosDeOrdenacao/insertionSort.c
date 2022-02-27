153c29a4771a82e0bdb7e5712748b8e814e0370d
Wesley Santos <wesleysotnas64@gmail.com>
2022-02-27 10:29:37 -0300

    Update insertionSort.c

--- AlgoritmosDeOrdenacao/insertionSort.c
+++ AlgoritmosDeOrdenacao/insertionSort.c
@@ -4,22 +4,23 @@
 #define TAMANHO 10
 
 void insertionSort(int vetor[], int tamanho);
+void troca(int vetor[], int posi1, int posi2);
 void imprimeVetor(int vetor[], int tamanho);
 
 int main(){
 
     int vetor[TAMANHO];
 
-    vetor[0] = 33;
-    vetor[1] = 12;
-    vetor[2] = 99;
-    vetor[3] = 54;
-    vetor[4] = 42;
-    vetor[5] = 87;
-    vetor[6] = 15;
-    vetor[7] = 10;
-    vetor[8] = 96;
-    vetor[9] = 81;
+    vetor[0] = 10;
+    vetor[1] = 9;
+    vetor[2] = 8;
+    vetor[3] = 7;
+    vetor[4] = 6;
+    vetor[5] = 5;
+    vetor[6] = 4;
+    vetor[7] = 3;
+    vetor[8] = 2;
+    vetor[9] = 1;
 
     imprimeVetor(vetor, TAMANHO);
 
@@ -31,18 +32,17 @@ int main(){
 
 
 void insertionSort(int vetor[], int tamanho){//O(n^2)
-    int aux;
     for(int i = 1; i < tamanho; i++){
-        aux = vetor[i];
         for(int j = i; j > 0; j--){
-            if(aux < vetor[j-1]){
-                vetor[j] = vetor[j-1];
-            } else{
-                vetor[j] = aux;
-                break;
-            } 
+            if(vetor[j] < vetor[j-1]) troca(vetor, j, j-1);
         } 
     }
+}
+
+void troca(int vetor[], int posi1, int posi2){ //O(1)
+    int aux = vetor[posi1];
+    vetor[posi1] = vetor[posi2];
+    vetor[posi2] = aux;
 }
 
 void imprimeVetor(int vetor[], int tamanho){ //O(n)
